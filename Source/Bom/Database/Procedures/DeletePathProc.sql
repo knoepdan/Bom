6dd1104ba2dc3d13bbd69992c28ed31941d63423
knoepfda <daniel.knoepfel@live.de>
2019-09-26 23:28:04 +0200

    prepare delte SP

--- Source/Bom/Database/Procedures/DeletePathProc.sql
+++ Source/Bom/Database/Procedures/DeletePathProc.sql
@@ -1,7 +1,8 @@
 ﻿CREATE PROCEDURE dbo.[DeletePathProc]
 	@pathId AS INT NULL,
-	@alsoDeleteNode AS BIT  NULL,
-	@newMainPathId AS INT NULL = NULL
+	@deleteSubTree AS BIT  = 0
+--	,@alsoDeleteNode AS BIT  NULL
+--	,@newMainPathId AS INT NULL = NULL
 AS
 BEGIN
 
@@ -11,11 +12,11 @@ BEGIN
 		print 'passed param @pathId may not be NULL';
 		THROW 50001, 'passed param @pathId may not be NULL', 1;
 	END
-	IF (@alsoDeleteNode IS NULL)
-	BEGIN
-		print 'passed param @@alsoDeleteNode may not be NULL';
-		THROW 50002, 'passed param @@alsoDeleteNode may not be NULL', 1;
-	END
+	--IF (@alsoDeleteNode IS NULL)
+	--BEGIN
+	--	print 'passed param @@alsoDeleteNode may not be NULL';
+	--	THROW 50002, 'passed param @@alsoDeleteNode may not be NULL', 1;
+	--END
 	-- end params checking
 
 	BEGIN TRANSACTION;
@@ -26,36 +27,36 @@ BEGIN
 		SELECT  @nodeId = NodeId FROM dbo.[Path] WHERE PathId = @pathId
 
 		-- ## 1. Set mainPath Node
-		IF NOT @newMainPathId IS NULL 
-		BEGIN
-			IF NOT EXISTS (SELECT PathId FROM dbo.[Path] WHERE PathId = @newMainPathId AND NodeId = @nodeId)
-			BEGIN
-				print 'error 1. execution aborted';
-				THROW 50101, 'passed replacement pathId does not exist or does not belong to node', 1;
-			END
+		--IF NOT @newMainPathId IS NULL 
+		--BEGIN
+		--	IF NOT EXISTS (SELECT PathId FROM dbo.[Path] WHERE PathId = @newMainPathId AND NodeId = @nodeId)
+		--	BEGIN
+		--		print 'error 1. execution aborted';
+		--		THROW 50101, 'passed replacement pathId does not exist or does not belong to node', 1;
+		--	END
 
-			UPDATE dbo.[Node]
-				SET MainPathId = @newMainPathId
-				WHERE NodeId = @nodeId AND MainPathId = @pathId
-		END
-		ELSE
-		BEGIN
-			DECLARE @otherPathId INT
-			SET @otherPathId = 0
-			SELECT @otherPathId = PathId FROM dbo.[Path] WHERE PathId <> pathId AND NodeId = @nodeId
+		--	UPDATE dbo.[Node]
+		--		SET MainPathId = @newMainPathId
+		--		WHERE NodeId = @nodeId AND MainPathId = @pathId
+		--END
+		--ELSE
+		--BEGIN
+		--	DECLARE @otherPathId INT
+		--	SET @otherPathId = 0
+		--	SELECT @otherPathId = PathId FROM dbo.[Path] WHERE PathId <> pathId AND NodeId = @nodeId
 
-			IF @otherPathId > 0 
-				UPDATE dbo.[Node] SET MainPathId = @otherPathId WHERE NodeId = @nodeId AND MainPathId = @pathId
-			ELSE
-				IF @alsoDeleteNode = 0 
-				BEGIN
-					print 'error 2. execution aborted';
-					THROW 50104, 'No replacement for main path id found and node is not to be deleted.. will not work', 1;
-				END
-				-- set to null
-				UPDATE dbo.[Node] SET MainPathId = NULL WHERE NodeId = @nodeId AND MainPathId = @pathId
+		--	IF @otherPathId > 0 
+		--		UPDATE dbo.[Node] SET MainPathId = @otherPathId WHERE NodeId = @nodeId AND MainPathId = @pathId
+		--	ELSE
+		--		IF @alsoDeleteNode = 0 
+		--		BEGIN
+		--			print 'error 2. execution aborted';
+		--			THROW 50104, 'No replacement for main path id found and node is not to be deleted.. will not work', 1;
+		--		END
+		--		-- set to null
+		--		UPDATE dbo.[Node] SET MainPathId = NULL WHERE NodeId = @nodeId AND MainPathId = @pathId
 
-		END
+		--END
 
 		-- ## 2. Move all the children to parent
 		-- TODO - check what children would become new root paths
@@ -73,31 +74,41 @@ BEGIN
 				THROW 50151, 'root was about to be deleted but had children. First delete children before deleting before root node', 1;
 		END
 
-		-- loop over all child nodes and call sp recursivly
-		DECLARE @childPathId INT
-		DECLARE db_cursor CURSOR FOR 
-		SELECT PathId FROM dbo.[Path]
-			WHERE NodePath.IsDescendantOf(@currentPath) = 1
-			AND [Level] =  (@currentLevel + 1)
-
-		OPEN db_cursor  
-			FETCH NEXT FROM db_cursor INTO @childPathId  
-
-			WHILE @@FETCH_STATUS = 0  
-			BEGIN  
-				EXEC dbo.MoveNodeProc @pathId = @childPathId, @newParentPathId = @parentPathId, @moveChildrenToo = 1
-				FETCH NEXT FROM db_cursor INTO @childPathId 
-			END 
-
-		CLOSE db_cursor  
-		DEALLOCATE db_cursor 
-	
-		-- ## 3. Delete Path (and node)
-		DELETE FROM [dbo].[Path] WHERE PathId = @pathId
-		IF @alsoDeleteNode = 0 
+		IF @deleteSubTree = 0
 		BEGIN
-			DELETE FROM dbo.[Node] WHERE NodeId = @nodeId
+
+			-- loop over all child nodes and call sp recursivly
+			DECLARE @childPathId INT
+			DECLARE db_cursor CURSOR FOR 
+			SELECT PathId FROM dbo.[Path]
+				WHERE NodePath.IsDescendantOf(@currentPath) = 1
+				AND [Level] =  (@currentLevel + 1)
+
+			OPEN db_cursor  
+				FETCH NEXT FROM db_cursor INTO @childPathId  
+
+				WHILE @@FETCH_STATUS = 0  
+				BEGIN  
+					EXEC dbo.MoveNodeProc @pathId = @childPathId, @newParentPathId = @parentPathId, @moveChildrenToo = 1
+					FETCH NEXT FROM db_cursor INTO @childPathId 
+				END 
+
+			CLOSE db_cursor  
+			DEALLOCATE db_cursor 
+	
+			-- ## 3. Delete Path (and node)
+			DELETE FROM [dbo].[Path] WHERE PathId = @pathId
 		END
+		ELSE
+		BEGIN
+			-- DELETE all descendants too
+			DELETE FROM [dbo].[Path] WHERE  NodePath.IsDescendantOf(@currentPath) = 1 
+			DELETE FROM [dbo].[Path] WHERE PathId = @pathId
+		END
+		--IF @alsoDeleteNode = 0 
+		--BEGIN
+		--	DELETE FROM dbo.[Node] WHERE NodeId = @nodeId
+		--END
 
 		COMMIT TRANSACTION 
     END TRY
