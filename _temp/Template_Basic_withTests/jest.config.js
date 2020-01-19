3de9f300b760b58c61d0a4e904fb2d0b2f7dbea7
knoepfda <daniel.knoepfel@live.de>
2020-01-19 22:30:23 +0100

    react templates stuff (with tests)

---
+++ _temp/Template_Basic_withTests/jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  
+	// Setup Enzyme  (see src/setupEnzyme.ts)
+	"snapshotSerializers": ["enzyme-to-json/serializer"],
+    setupFilesAfterEnv: ['<rootDir>/src/setupEnzyme.ts'],
+	// "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts", depreceated in favor of setupFilesAfterEnv
+ 
+  
+}
+
+/*
+
+https://github.com/cedrickchee/react-typescript-jest-enzyme-testing
+...
+The testRegex tells Jest to look for tests in any __tests__ folder AND also any files anywhere that use the (.test|.spec).(ts|tsx) extension e.g. checkbox.test.tsx etc.
+..
+*/
