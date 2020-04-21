e6c1629688b4156ca6d40029ae944ad58b749223
knoepfda <daniel.knoepfel@live.de>
2020-04-21 22:03:46 +0200

    react template: global css example (still using css modules)

--- _temp/Basic_Template_withHookstate/src/main.tsx
+++ _temp/Basic_Template_withHookstate/src/main.tsx
@@ -1,3 +1,10 @@
+// include css in top component in correct order (to mimick a global css)
+// styles have to be applied value. Example:  className={macroCss.pt2}
+import normCss from 'app/style/global.normalize.css';
+import macroCss from 'app/style/global.macros.css';
+import defaultCss from 'app/style/global.css';
+
+// app
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Hello } from 'app/components/Hello';
@@ -13,16 +20,20 @@ const imgStyle = {
     maxHeight: '40px',
     border: '5px solid pink',
 };
+// hack to ensure css that is to be global is loaded and no type errors
+if (false && normCss && macroCss && defaultCss) {
+    console.log('');
+}
 
 ReactDOM.render(
-    <div>
+    <div className={macroCss.solidBox + ' ' + macroCss.m5 + ' ' + macroCss.p10}>
         <Hello compiler="Typescript" framework="React..." bundler="Webpack" />
         <ReactHooksExample />
         <br />
         <ReactWrapperExample />
         <br />
         <br />
-        <div>
+        <div className={macroCss.dottedBox + ' ' + macroCss.p5}>
             <img src={samplePng} alt="samplePng" />
             <img src={sampleJpg} alt="sampleJpg" />
             <img src={sampleGif} alt="sampleGif" style={{ maxHeight: 50 }} />
