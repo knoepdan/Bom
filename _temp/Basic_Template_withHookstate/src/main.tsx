2dfd0b9c2c94c30f503f1b5026225ce0e465df02
knoepfda <daniel.knoepfel@live.de>
2020-04-21 21:44:53 +0200

    React template: Support and examples for images

--- _temp/Basic_Template_withHookstate/src/main.tsx
+++ _temp/Basic_Template_withHookstate/src/main.tsx
@@ -4,14 +4,15 @@ import { Hello } from 'app/components/Hello';
 import { ReactHooksExample } from 'app/components/ReactHooksExample';
 import { ReactWrapperExample } from 'app/components/ReactWrapperExample';
 
-//import { createBrowserHistory } from 'history';
-// import { configureStore } from 'app/store';
-//import { Router } from 'react-router';
-//import { App } from './app';
+import samplePng from 'app/img/samplePng.png';
+import sampleJpg from 'app/img/sampleJpg.jpg';
+import sampleGif from 'app/img/sampleGif.gif';
+import sampleSvg from 'app/img/sampleSvg.svg';
 
-// prepare store
-//const history = createBrowserHistory();
-//const store = configureStore();
+const imgStyle = {
+    maxHeight: '40px',
+    border: '5px solid pink',
+};
 
 ReactDOM.render(
     <div>
@@ -19,16 +20,14 @@ ReactDOM.render(
         <ReactHooksExample />
         <br />
         <ReactWrapperExample />
+        <br />
+        <br />
+        <div>
+            <img src={samplePng} alt="samplePng" />
+            <img src={sampleJpg} alt="sampleJpg" />
+            <img src={sampleGif} alt="sampleGif" style={{ maxHeight: 50 }} />
+            <img src={sampleSvg} alt="sampleSvg" style={imgStyle} />
+        </div>
     </div>,
     document.getElementById('root'),
 );
-/*
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
-*/
--- _temp/Basic_Template_withHookstate/types/global.d.ts
+++ _temp/Basic_Template_withHookstate/types/global.d.ts
@@ -2,8 +2,33 @@
 
 // for style loader
 declare module '*.css' {
-  const styles: any;
-  export = styles;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const styles: any;
+    export = styles;
+}
+
+declare module '*.jpg' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const styles: any;
+    export = styles;
+}
+
+declare module '*.png' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const styles: any;
+    export = styles;
+}
+
+declare module '*.svg' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const styles: any;
+    export = styles;
+}
+
+declare module '*.gif' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const styles: any;
+    export = styles;
 }
 
 // Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
