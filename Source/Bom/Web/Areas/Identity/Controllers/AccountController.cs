65be3df4495659ceab5149d7116cc459233365ff
knoepfda <daniel.knoepfel@live.de>
2021-01-12 22:16:11 +0100

    some razor/html refactoring

--- Source/Bom/Web/Areas/Identity/Controllers/AccountController.cs
+++ Source/Bom/Web/Areas/Identity/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Bom.Core.Data;
 using Bom.Web.Areas.Main.Models;
 using Bom.Web.Lib.Infrastructure;
 using Bom.Web.Areas.Identity.Models;
+using Bom.Core.Model.Identity;
 
 namespace Bom.Web.Areas.Main.Controllers
 {
@@ -32,6 +33,32 @@ namespace Bom.Web.Areas.Main.Controllers
             var model = new RegisterVm();
 
             return View( model);
+            //  return View("~/Areas/Identity/Views/Account/Register", model);
+        }
+
+        [HttpPost("register")]
+        //  public async Task<IActionResult> Register(RegisterVm model)
+        public IActionResult Register(RegisterVm model)
+        {
+
+            if (this.ModelState.IsValid)
+            {
+                // TODO -> validate confirm pw, validate if email is unique and return proper error message
+
+
+                var user = new User();
+                user.Username = model.Username;
+                user.Salt = "bla";
+                user.PasswordHash = "xx";
+               // this._context.Users.Add(user);
+
+              //  await this._context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return View(model);
             //  return View("~/Areas/Identity/Views/Account/Register", model);
         }
 
--- Source/Bom/Web/Areas/Identity/Models/RegisterVm.cs
+++ Source/Bom/Web/Areas/Identity/Models/RegisterVm.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bom.Web.Areas.Identity.Models
 {
     public class RegisterVm
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
         public string Username { get; set; } = "";
 
+        [Required]
+        [StringLength(255)]
         public string? Password { get; set; } = "";
 
+        [Required]
         public string? ConfirmPassword { get; set; } = "";
     }
 }
