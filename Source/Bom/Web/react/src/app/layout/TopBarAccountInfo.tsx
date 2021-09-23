42d49ccf48d14c7582c9e12107865d682e7bd365
knoepfda <daniel.knoepfel@dormakaba.com>
2021-09-23 22:55:56 +0200

    #logout user (temporary.. but makes testing/developing easier)

--- Source/Bom/Web/react/src/app/layout/TopBarAccountInfo.tsx
+++ Source/Bom/Web/react/src/app/layout/TopBarAccountInfo.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as state from 'app/common/UserState';
 import * as nav from 'app/common/NavigationState';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // useHistory
 import css from './TopBarAccountInfo.module.css';
 import { useState as globalState } from '@hookstate/core';
 
@@ -11,14 +11,40 @@ interface Props {}
 // more or less random notes about webpack setup
 export const TopBarAccountInfo = (): React.ReactElement<Props> => {
     const userState = globalState(state.userStateRef);
-    const history = useHistory(); // https://stackoverflow.com/questions/31079081/programmatically-navigate-using-react-router
+    //const history = useHistory(); // https://stackoverflow.com/questions/31079081/programmatically-navigate-using-react-router
 
-    const handleLogoutClick = (): void => {
+    // TODO this is a hack
+    const handleLogoutClick = async (): Promise<void> => {
+        const url = 'en/account/logout';
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                cache: 'no-cache',
+            });
+            console.log('finished calling logut' + response);
+            if (response) {
+                console.log('finished calling logut' + response.status + '          ' + response.statusText);
+            }
+            if (response && response.status >= 200 && response.status <= 400) {
+
+                // reset local state
+                state.userStateRef.set((userModel) => {
+                    userModel.logOut();
+                    return userModel;
+                });
+            }
+        } catch (e : any) {
+            debugger;
+            console.log('error: ' + e.message);
+        }
+        window.document.location.reload();
+        /*
         state.userStateRef.set((userModel) => {
             userModel.logOut();
             history.push(nav.Routes.PublicMainTo);
             return userModel;
         });
+        */
     };
 
     let loginInfo;
